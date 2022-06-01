2853aade8032d4978d9ab6053f897df39bd32c00
ezgiksrci <keserci.ezgi@gmail.com>
2022-06-01 22:27:43 +0300

    Audio Manager Eklemeleri
    Master Sound Mixer düzenlendi, mute/unmute toggle butonuna fonksiyon  eklendi.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,16 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioMixer masterMixer;
     [SerializeField] AudioMixer sfxAudioMixer;
     [SerializeField] AudioMixer musicMixer;
+    [SerializeField] Toggle muteToggle;
+
+
     public void SetVolumeMaster(float volume)
     {
         masterMixer.SetFloat("volume", volume);
+        sfxAudioMixer.SetFloat("volume", volume);
+        musicMixer.SetFloat("volume", volume);
     }
 
     public void SetVolumeSFX(float volume)
@@ -21,5 +25,21 @@ public class AudioManager : MonoBehaviour
     public void SetVolumeMusic(float volume)
     {
         musicMixer.SetFloat("volume", volume);
+    }
+
+    public void MuteOrUnmute()
+    {
+        if (muteToggle.isOn)
+        {
+            masterMixer.SetFloat("volume", -60f);
+            sfxAudioMixer.SetFloat("volume", -60f);
+            musicMixer.SetFloat("volume", -60f);
+        }
+        else
+        {
+            masterMixer.SetFloat("volume", 0f);
+            sfxAudioMixer.SetFloat("volume", 0f);
+            musicMixer.SetFloat("volume", 0f);
+        }
     }
 }
