64810ff665a06339a0b7203200e8ccdf87250651
ezgiksrci <keserci.ezgi@gmail.com>
2022-06-02 16:10:04 +0300

    Revert "Merge remote-tracking branch 'origin/feat-taha2' into feat-ezgi"
    This reverts commit 72eb06b09f7373a67ad03c9264739b856368ebe8, reversing
    changes made to 0be05f5a289b37b2133858469ac23386f0a1665b.

--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -7,22 +7,9 @@ public class SpawnPlayers : MonoBehaviour
 {
     public GameObject PlayerPrefab;
     PhotonView view;
-    public static int pos;
-    public static bool curveFollowAllowed = false;
-    public static int numberPlayers = 0;
 
     private void Start()
     {
-        numberPlayers = PhotonNetwork.CountOfPlayers;
-        if (numberPlayers == 1)
-        {
-            PhotonNetwork.Instantiate(PlayerPrefab.name, transform.position + transform.right * -2, Quaternion.identity);
-            pos = -1;
-        }
-        else if (numberPlayers == 2)
-        {
-            PhotonNetwork.Instantiate(PlayerPrefab.name, transform.position + transform.right * 2, Quaternion.identity);
-            pos = 1;
-        }
+        PhotonNetwork.Instantiate(PlayerPrefab.name, transform.position + transform.right * 2, Quaternion.identity);
     }
 }
--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -2,43 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using Photon.Realtime;
 
-public class camera : MonoBehaviourPunCallbacks
+public class camera : MonoBehaviour
 {
     PhotonView view;
-    int numberPlayers = 1;
     void Start()
     {
         view = GetComponent<PhotonView>();
-
 
         if (view.IsMine)
         {
             gameObject.transform.Find("Camera").gameObject.SetActive(true);
             gameObject.transform.Find("Canvas").gameObject.SetActive(true);
         }
-    }
-
-    private void Update()
-    {
-        if (PhotonNetwork.PlayerList.Length == 2)
-        {
-            new WaitForSeconds(3);
-            PhotonView photonView = PhotonView.Get(this);
-            photonView.RPC("curveFollowEnabled", RpcTarget.All);
-        }
-    }
-
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        numberPlayers++;
-    }
-
-    [PunRPC]
-    void curveFollowEnabled()
-    {
-        gameObject.GetComponent<curveFollow>().enabled = true;
-        gameObject.GetComponent<camera>().enabled = false;
     }
 }
