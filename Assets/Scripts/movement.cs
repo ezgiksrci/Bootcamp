885f22c8d606a92ad6ad97eb875003abd8665974
ezgiksrci <keserci.ezgi@gmail.com>
2022-06-03 15:20:36 +0300

    Swipe control eklendi.

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -9,7 +9,13 @@ public class movement : MonoBehaviour
     [SerializeField] public static int pos;             // -1: left, 0: middle(default); 1:right
     int sign;             // Direction of movement
     bool speedCheck = false;
+
+    private Vector2 startTouchPosition;
+    private Vector2 endTouchPosition;
+
     PhotonView view;
+
+
 
     private void Start()
     {
@@ -18,20 +24,44 @@ public class movement : MonoBehaviour
     }
 
     void Update()
-    {
+    {        
         if (view.IsMine)
         {
-            // Determines which line the player swipe
-            if (Input.GetKeyDown(KeyCode.A) && pos > -1)
+            // Swipe controls for mobile.
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                pos--;
-                sign = -1;
+                startTouchPosition = Input.GetTouch(0).position;
             }
-            else if (Input.GetKeyDown(KeyCode.D) && pos < 1)
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
             {
-                pos++;
-                sign = 1;
+                endTouchPosition = Input.GetTouch(0).position;
+                if (endTouchPosition.x < startTouchPosition.x && pos > -1)
+                {
+                    pos--;
+                    sign = -1;
+                }
+                else if (endTouchPosition.x > startTouchPosition.x && pos < 1)
+                {
+                    pos++;
+                    sign = 1; 
+                }
             }
+
+
+
+            //// Determines which line the player swipe
+            //if (Input.GetKeyDown(KeyCode.A) && pos > -1)
+            //{
+            //    pos--;
+            //    sign = -1;
+            //}
+            //else if (Input.GetKeyDown(KeyCode.D) && pos < 1)
+            //{
+            //    pos++;
+            //    sign = 1;
+            //}
 
             //if (Input.GetKey(KeyCode.W))
             //{
